1b7131dc506b263e3ee7ed9fead6c5afe917257d
iammantas <nacickasmantas@gmail.com>
2020-05-30 18:33:35 +0300

    ReversedLinkedList

--- src/MainDFS.java
+++ src/MainDFS.java
@@ -1,28 +1,55 @@
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.LinkedHashMap;
-import java.util.TreeMap;
+class LinkedList {
 
-public class MainDFS {
+    static Node head;
 
-    public static void main(String [] args) {
+    static class Node {
 
-        long startTime=System.nanoTime();
+        int data;
+        Node next;
 
-        Hashtable<String, String> hashTable=new Hashtable<String, String>();
-
-        String first="m";
-        String second="n";
-
-        for(int i=0; i<2000; i++) {
-            first=first+"ms";
-            second=second+"n";
-            hashTable.put(first,second);
+        Node(int d)
+        {
+            data = d;
+            next = null;
         }
-
-        long endTime=System.nanoTime();
-        long timeElapsed=endTime-startTime;
-        System.out.println(timeElapsed);
-
     }
+
+    Node reverse(Node node)
+    {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
     }
+
+    void printList(Node node)
+    {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+        list.head = new Node(85);
+        list.head.next = new Node(15);
+        list.head.next.next = new Node(4);
+        list.head.next.next.next = new Node(20);
+
+        System.out.println("Given Linked list");
+        list.printList(head);
+        head = list.reverse(head);
+        System.out.println("");
+        System.out.println("Reversed linked list ");
+        list.printList(head);
+    }
+} 
