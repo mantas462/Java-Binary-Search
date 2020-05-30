d32d410ea7f9c799c140d1c754efd70a862aad23
iammantas <nacickasmantas@gmail.com>
2020-05-30 18:44:21 +0300

    MergeSort

--- src/MainDFS.java
+++ src/MainDFS.java
@@ -1,44 +1,69 @@
-class QuickSort
+class MergeSort
 {
 
-    int partition(int arr[], int low, int high)
+    void merge(int arr[], int l, int m, int r)
     {
-        int pivot = arr[high];
-        int i = (low-1);
-        for (int j=low; j<high; j++)
+
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+
+
+
+        int i = 0, j = 0;
+
+
+        int k = l;
+        while (i < n1 && j < n2)
         {
-
-            if (arr[j] < pivot)
+            if (L[i] <= R[j])
             {
+                arr[k] = L[i];
                 i++;
-
-
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
             }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
         }
 
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
 
-        int temp = arr[i+1];
-        arr[i+1] = arr[high];
-        arr[high] = temp;
-
-        return i+1;
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
 
 
-
-    void sort(int arr[], int low, int high)
+    void sort(int arr[], int l, int r)
     {
-        if (low < high)
+        if (l < r)
         {
+            int m = (l+r)/2;
 
-            int pi = partition(arr, low, high);
+            sort(arr, l, m);
+            sort(arr , m+1, r);
 
-
-            sort(arr, low, pi-1);
-            sort(arr, pi+1, high);
+            merge(arr, l, m, r);
         }
     }
 
@@ -46,20 +71,21 @@ class QuickSort
     {
         int n = arr.length;
         for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
         System.out.println();
     }
 
-
     public static void main(String args[])
     {
-        int arr[] = {10, 7, 8, 9, 1, 5};
-        int n = arr.length;
+        int arr[] = {12, 11, 13, 5, 6, 7};
 
-        QuickSort ob = new QuickSort();
-        ob.sort(arr, 0, n-1);
+        System.out.println("Given Array");
+        printArray(arr);
 
-        System.out.println("sorted array");
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length-1);
+
+        System.out.println("\nSorted array");
         printArray(arr);
     }
-} 
+}
