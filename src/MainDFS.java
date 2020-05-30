bbd1f562ee9fe890fb4460e690dd722ff2bae4e3
iammantas <nacickasmantas@gmail.com>
2020-05-30 18:59:38 +0300

    BubbleSort

--- src/MainDFS.java
+++ src/MainDFS.java
@@ -1,73 +1,19 @@
-class MergeSort
+class BubbleSort
 {
-
-    void merge(int arr[], int l, int m, int r)
+    void bubbleSort(int arr[])
     {
-
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-
-        int L[] = new int [n1];
-        int R[] = new int [n2];
-
-
-        for (int i=0; i<n1; ++i)
-            L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
-
-
-
-        int i = 0, j = 0;
-
-
-        int k = l;
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= R[j])
-            {
-                arr[k] = L[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < n1)
-        {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2)
-        {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
     }
 
-
-    void sort(int arr[], int l, int r)
-    {
-        if (l < r)
-        {
-            int m = (l+r)/2;
-
-            sort(arr, l, m);
-            sort(arr , m+1, r);
-
-            merge(arr, l, m, r);
-        }
-    }
-
-    static void printArray(int arr[])
+    void printArray(int arr[])
     {
         int n = arr.length;
         for (int i=0; i<n; ++i)
@@ -77,15 +23,9 @@ class MergeSort
 
     public static void main(String args[])
     {
-        int arr[] = {12, 11, 13, 5, 6, 7};
-
-        System.out.println("Given Array");
-        printArray(arr);
-
-        MergeSort ob = new MergeSort();
-        ob.sort(arr, 0, arr.length-1);
-
-        System.out.println("\nSorted array");
-        printArray(arr);
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
     }
-}
