ef722fef00db1bc5cd2b5ba19176eb12cc204e66
iammantas <nacickasmantas@gmail.com>
2020-05-30 19:01:37 +0300

    Recursion

--- src/MainDFS.java
+++ src/MainDFS.java
@@ -1,31 +1,22 @@
-class BubbleSort
-{
-    void bubbleSort(int arr[])
+class GFG {
+    static void printFun(int test)
     {
-        int n = arr.length;
-        for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
-                if (arr[j] > arr[j+1])
-                {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+        if (test < 1)
+            return;
+
+        else {
+            System.out.printf("%d ", test);
+
+            printFun(test - 1);
+
+            System.out.printf("%d ", test);
+            return;
+        }
     }
 
-    void printArray(int arr[])
+    public static void main(String[] args)
     {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
+        int test = 3;
+        printFun(test);
     }
-
-    public static void main(String args[])
-    {
-        BubbleSort ob = new BubbleSort();
-        int arr[] = {64, 34, 25, 12, 22, 11, 90};
-        ob.bubbleSort(arr);
-        System.out.println("Sorted array");
-        ob.printArray(arr);
-    }
+}
